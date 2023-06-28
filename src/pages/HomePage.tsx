0251e259793f7cbb2cf1a0e15358a66afbe241eb
Vadym Frolov <vdfroloff@gmail.com>
2023-06-28 21:42:05 +0300

    WORK:
    - models
    - github.api

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSearchUsersQuery } from '../store/github/github.api';
 
 export const HomePage = () => {
-  const { isLoading, data } = useSearchUsersQuery('vadymfrlv');
-  return <div>HomePage</div>;
+  const [search, setSearch] = useState('');
+
+  const { isLoading, isError, data } = useSearchUsersQuery(search);
+
+  return (
+    <div className="relative flex justify-center pt-10 mx-auto h-screen w-screen ">
+      {isError && (
+        <p className="absolute top-[110px] text-center uppercase text-red-500">
+          Something went wrong
+        </p>
+      )}
+
+      <div className="relative flex justify-center w-[560px]">
+        <input
+          type="text"
+          placeholder="GitHub username..."
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          className="block w-[230px] h-[30px] rounded-md border-0 py-1.5 pl-3 pr-3 text-[#3c3c3c] ring-2 ring-inset ring-[#c3f51d] sm:text-sm sm:leading-6 shadow-sm"
+        />
+
+        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-[red]">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aliquam.
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,18 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+import { ServerResponse, User } from '../../models/models';
+
 export const githubApi = createApi({
   reducerPath: 'github/api',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.github.com/',
   }),
   endpoints: build => ({
-    searchUsers: build.query<any, string>({
+    searchUsers: build.query<User[], string>({
       query: (search: string) => ({
         url: 'search/users',
         params: {
           q: search,
+          per_page: 10,
         },
       }),
+      transformResponse: (response: ServerResponse<User>) => response.items,
     }),
   }),
 });

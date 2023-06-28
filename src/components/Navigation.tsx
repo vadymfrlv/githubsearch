c191a9e4efd4be3fed9b2b91dc429647d8eb59f8
Vadym Frolov <vdfroloff@gmail.com>
2023-06-28 13:34:34 +0300

    WORK:
    - navigation style

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 export const Navigation = () => {
   return (
-    <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
-      <h2>Search</h2>
+    <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-green-600 text-yellow-300">
+      <h2 className="text-xl font-semibold tracking-widest">RC + TS + TW</h2>
 
       <span>
-        <Link to={'/'} className="mr-2">
+        <Link to={'/'} className="mr-2 font-semibold text-lg">
           Home
         </Link>
-        <Link to={'/favorites'}>Favorites</Link>
+        <Link to={'/favorites'} className="font-semibold text-lg">
+          Favorites
+        </Link>
       </span>
     </nav>
   );
